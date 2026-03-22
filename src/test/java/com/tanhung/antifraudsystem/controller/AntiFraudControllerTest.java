2ba0f14c7e803121023c0b9bd0f57d51e4e27502
tanhn2-uci <tanhn2@uci.edu>
2026-03-21 21:06:59 -0700

    Finish writing integration test cases for AntiFraudController

---
+++ src/test/java/com/tanhung/antifraudsystem/controller/AntiFraudControllerTest.java
@@ -0,0 +1,153 @@
+package com.tanhung.antifraudsystem.controller;
+
+import com.tanhung.antifraudsystem.dto.response.ActionResponse;
+import com.tanhung.antifraudsystem.exceptionHandler.GlobalExceptionHandler;
+import com.tanhung.antifraudsystem.service.AntiFraudService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.swing.*;
+import java.math.BigDecimal;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AntiFraudController.class)
+@Import(GlobalExceptionHandler.class)
+class AntiFraudControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private AntiFraudService antiFraudService;
+
+    @Test
+    @WithMockUser
+    void shouldReturnAllowed_whenRequestAtLeastOneDollarAndLessThanOrEqualTwoHundredDollars() throws Exception{
+        String json = """
+                {
+                    "amount" : 200
+                }
+                """;
+
+        ActionResponse expected = new ActionResponse("ALLOWED");
+        BigDecimal amount = new BigDecimal("200");
+        Mockito.when(antiFraudService.checkAmount(amount))
+                .thenReturn(expected);
+
+        mockMvc.perform(post("/api/antifraud/transaction")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.result").value("ALLOWED"));
+
+        Mockito.verify(antiFraudService).checkAmount(amount);
+    }
+
+    @Test
+    @WithMockUser
+    void shouldReturnManualProcessing_whenRequestOverTwoHundredAndLessThanOrEqualFifteenthHundred() throws Exception{
+        String json = """
+                {
+                    "amount" : 1000
+                }
+                """;
+        BigDecimal amount = new BigDecimal("1000");
+        ActionResponse expected = new ActionResponse("MANUAL_PROCESSING");
+        Mockito.when(antiFraudService.checkAmount(amount))
+                .thenReturn(expected);
+
+        mockMvc.perform(post("/api/antifraud/transaction")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.result").value("MANUAL_PROCESSING"));
+
+        Mockito.verify(antiFraudService).checkAmount(amount);
+    }
+
+    @Test
+    @WithMockUser
+    void shouldReturnProhibited_whenRequestOverFifteenthHundredDollars() throws Exception{
+        String json = """
+                {
+                    "amount" : 1700
+                }
+                """;
+        BigDecimal amount = new BigDecimal("1700");
+        ActionResponse expected = new ActionResponse("PROHIBITED");
+        Mockito.when(antiFraudService.checkAmount(amount))
+                .thenReturn(expected);
+
+        mockMvc.perform(post("/api/antifraud/transaction")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.result").value("PROHIBITED"));
+
+        Mockito.verify(antiFraudService).checkAmount(amount);
+    }
+
+    @Test
+    @WithMockUser
+    void shouldReturnBadRequest_whenRequestAmountLessThanOneDollar() throws Exception{
+        String json = """
+                {
+                    "amount" : -100
+                }
+                """;
+
+        mockMvc.perform(post("/api/antifraud/transaction")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isBadRequest());
+
+        Mockito.verifyNoInteractions(antiFraudService);
+    }
+
+    @Test
+    @WithMockUser
+    void shouldReturnBadRequest_whenRequestAmountIsMissing() throws Exception{
+        String json = """
+                {
+                }
+                """;
+
+        mockMvc.perform(post("/api/antifraud/transaction")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isBadRequest());
+
+        Mockito.verifyNoInteractions(antiFraudService);
+    }
+
+    @Test
+    void shouldReturnForbidden_whenFailToAuthenticate() throws Exception{
+        String json = """
+                {
+                    "amount" : 100
+                }
+                """;
+
+        mockMvc.perform(post("/api/antifraud/transaction")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isUnauthorized());
+    }
+}
