ec1d293e52962de69e2fa8cc0684b0877be62bb2
tanhn2-uci <tanhn2@uci.edu>
2026-07-02 14:24:34 -0700

    Add SuspiciousIpService

---
+++ src/main/java/com/tanhung/antifraudsystem/service/SuspiciousIpService.java
@@ -0,0 +1,51 @@
+package com.tanhung.antifraudsystem.service;
+
+import com.tanhung.antifraudsystem.dto.request.SuspiciousIpRequestDto;
+import com.tanhung.antifraudsystem.dto.response.IPResponseDto;
+import com.tanhung.antifraudsystem.exception.IPAddressConflictException;
+import com.tanhung.antifraudsystem.exception.IPAddressNotFoundException;
+import com.tanhung.antifraudsystem.mapper.SuspiciousIpAddressMapper;
+import com.tanhung.antifraudsystem.model.SuspiciousIPAddress;
+import com.tanhung.antifraudsystem.repo.SuspiciousIPRepo;
+import com.tanhung.antifraudsystem.validators.SuspiciousIpValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SuspiciousIpService {
+    private final SuspiciousIPRepo suspiciousIPRepo;
+    private final SuspiciousIpAddressMapper suspiciousIpAddressMapper;
+    private final SuspiciousIpValidator suspiciousIpValidator;
+
+    public SuspiciousIPAddress addIp(SuspiciousIpRequestDto requestIp) throws IPAddressConflictException{
+        String ipAddress = requestIp.getIpAddress();
+        if(suspiciousIpValidator.isSuspiciousIp(ipAddress)){
+            throw new IPAddressConflictException(ipAddress + " already exists in the suspicious ip list!", HttpStatus.CONFLICT);
+        }
+        SuspiciousIPAddress ipEntity = suspiciousIpAddressMapper.toEntity(requestIp);
+        return suspiciousIPRepo.save(ipEntity);
+    }
+
+    public void deleteIp(String ip){
+        if(suspiciousIpValidator.isSuspiciousIp(ip)){
+            throw new IPAddressNotFoundException(ip + " not found!", HttpStatus.NOT_FOUND);
+        }
+        suspiciousIPRepo.deleteByIpAddress(ip);
+    }
+
+    public IPResponseDto convertToDto(SuspiciousIPAddress ipAddress){
+        return suspiciousIpAddressMapper.toDto(ipAddress);
+    }
+
+    public List<IPResponseDto> getAllSuspiciousIps(){
+        return suspiciousIPRepo.findAll(Sort.by("id"))
+                .stream()
+                .map(suspiciousIpAddressMapper::toDto)
+                .toList();
+    }
+}
