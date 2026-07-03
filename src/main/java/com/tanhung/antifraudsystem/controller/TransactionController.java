015a6c8ec5eb10946be86dbea3f7fb74a95c9c6e
tanhn2-uci <tanhn2@uci.edu>
2026-07-02 17:14:01 -0700

    Finish spliting

--- src/main/java/com/tanhung/antifraudsystem/controller/TransactionController.java
+++ src/main/java/com/tanhung/antifraudsystem/controller/TransactionController.java
@@ -1,4 +1,27 @@
 package com.tanhung.antifraudsystem.controller;
 
+import com.tanhung.antifraudsystem.dto.request.TransactionRequestDto;
+import com.tanhung.antifraudsystem.dto.response.ActionResponseDto;
+import com.tanhung.antifraudsystem.service.TransactionReviewService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/antifraud")
 public class TransactionController {
+
+    private final TransactionReviewService transactionReviewService;
+
+    @Autowired
+    public TransactionController(TransactionReviewService service){
+        this.transactionReviewService = service;
+    }
+
+    @PostMapping("/transaction")
+    public ResponseEntity<ActionResponseDto> requestTransaction(@RequestBody @Valid TransactionRequestDto transactionRequestDto){
+        ActionResponseDto result = transactionReviewService.reviewRequest(transactionRequestDto);
+        return ResponseEntity.ok(result);
+    }
 }
--- src/main/java/com/tanhung/antifraudsystem/service/TransactionReviewService.java
+++ src/main/java/com/tanhung/antifraudsystem/service/TransactionReviewService.java
@@ -1,4 +1,49 @@
 package com.tanhung.antifraudsystem.service;
 
+import com.tanhung.antifraudsystem.dto.request.TransactionRequestDto;
+import com.tanhung.antifraudsystem.dto.response.ActionResponseDto;
+import com.tanhung.antifraudsystem.exception.InvalidCardNumberException;
+import com.tanhung.antifraudsystem.exception.InvalidRegionException;
+import com.tanhung.antifraudsystem.model.Region;
+import com.tanhung.antifraudsystem.model.Transaction;
+import com.tanhung.antifraudsystem.repo.RegionRepo;
+import com.tanhung.antifraudsystem.validators.CardNumberValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class TransactionReviewService {
+
+    private final RegionRepo regionRepo;
+    private final TransactionManagerService transactionManager;
+
+    public ActionResponseDto reviewRequest(TransactionRequestDto request){
+        validateCardNumberIsValid(request.getCardNumber());
+        return proceedTransactionRequest(request);
+    }
+
+    private void validateCardNumberIsValid(String cardNumber){
+        if(!CardNumberValidator.isValidCardNumber(cardNumber)){
+            throw new InvalidCardNumberException("Card number is invalid!", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private ActionResponseDto proceedTransactionRequest(TransactionRequestDto requestDto){
+        Transaction transaction = transactionManager.convertToEntity(requestDto);
+        Region region = findRegion(requestDto.getRegion());
+        if(region == null){
+            throw new InvalidRegionException(requestDto.getRegion() + " is invalid!", HttpStatus.BAD_REQUEST);
+        }
+        transaction.setRegion(region);
+        ActionResponseDto response = transactionManager.proceedTransaction(transaction);
+        transactionManager.saveTransaction(transaction);
+        return response;
+    }
+
+    private Region findRegion(String region){
+        return regionRepo.findByCode(region);
+    }
+
 }
