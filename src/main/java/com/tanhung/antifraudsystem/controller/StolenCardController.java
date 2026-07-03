31b928f4e16d1c4081591c511bf610834fdd49c4
tanhn2-uci <tanhn2@uci.edu>
2026-07-02 17:26:33 -0700

    Finish splitting new stolen card service and controller

--- src/main/java/com/tanhung/antifraudsystem/controller/StolenCardController.java
+++ src/main/java/com/tanhung/antifraudsystem/controller/StolenCardController.java
@@ -1,4 +1,46 @@
 package com.tanhung.antifraudsystem.controller;
 
+import com.tanhung.antifraudsystem.dto.request.StolenCardNumberRequestDto;
+import com.tanhung.antifraudsystem.dto.response.StatusResponseDto;
+import com.tanhung.antifraudsystem.dto.response.StolenCardResponseDto;
+import com.tanhung.antifraudsystem.service.StolenCardAdminService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/antifraud/stolencard")
 public class StolenCardController {
+    private final StolenCardAdminService stolenCardAdminService;
+
+    @Autowired
+    public StolenCardController(StolenCardAdminService service){
+        this.stolenCardAdminService = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<StolenCardResponseDto> addStolenCardNumber(@RequestBody @Valid StolenCardNumberRequestDto card){
+        StolenCardResponseDto result = stolenCardAdminService.addStolenCardNumber(card);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    @DeleteMapping("/{number}")
+    public ResponseEntity<StatusResponseDto> deleteStolenCardNumber(
+            @PathVariable
+            @Pattern(regexp = "^\\d{16}$", message = "Card number must be exactly 16 digits") String number){
+
+        StatusResponseDto response = stolenCardAdminService.deleteStolenCardNumber(number);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StolenCardResponseDto>> getStolenCardList(){
+        List<StolenCardResponseDto> cards = stolenCardAdminService.getStolenCardList();
+        return ResponseEntity.ok(cards);
+    }
 }
--- src/main/java/com/tanhung/antifraudsystem/service/StolenCardAdminService.java
+++ src/main/java/com/tanhung/antifraudsystem/service/StolenCardAdminService.java
@@ -1,4 +1,59 @@
 package com.tanhung.antifraudsystem.service;
 
+import com.tanhung.antifraudsystem.dto.request.StolenCardNumberRequestDto;
+import com.tanhung.antifraudsystem.dto.response.StatusResponseDto;
+import com.tanhung.antifraudsystem.dto.response.StolenCardResponseDto;
+import com.tanhung.antifraudsystem.exception.InvalidCardNumberException;
+import com.tanhung.antifraudsystem.exception.StolenCardNullException;
+import com.tanhung.antifraudsystem.model.StolenCard;
+import com.tanhung.antifraudsystem.validators.CardNumberValidator;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class StolenCardAdminService {
+    private final StolenCardService stolenCardService;
+
+    @Transactional
+    public StolenCardResponseDto addStolenCardNumber(StolenCardNumberRequestDto stolenCard){
+        if(stolenCard == null){
+            throw new StolenCardNullException("Your card object must not be null!", HttpStatus.BAD_REQUEST);
+        }
+        StolenCard cardEntity = proceedAddStolenCardNumber(stolenCard);
+        return buildStolenCardResponse(cardEntity);
+    }
+
+    private StolenCard proceedAddStolenCardNumber(StolenCardNumberRequestDto requestCard){
+        if(!CardNumberValidator.isValidCardNumber(requestCard.getCardNumber())){
+            throw new InvalidCardNumberException(requestCard.getCardNumber() + " card number is invalid!", HttpStatus.BAD_REQUEST);
+        }
+        return stolenCardService.addCard(requestCard);
+    }
+
+    private StolenCardResponseDto buildStolenCardResponse(StolenCard stolenCard){
+        return stolenCardService.convertToDto(stolenCard);
+    }
+
+    @Transactional
+    public StatusResponseDto deleteStolenCardNumber(String cardNumber){
+        if(cardNumber == null) {
+            throw new StolenCardNullException("Your card number must not be null!", HttpStatus.BAD_REQUEST);
+        }
+        return proceedDeleteStolenCardNumber(cardNumber);
+    }
+
+    private StatusResponseDto proceedDeleteStolenCardNumber(String cardNumber){
+        stolenCardService.deleteCard(cardNumber);
+        return new StatusResponseDto("Card " + cardNumber + " successfully removed!");
+    }
+
+    public List<StolenCardResponseDto> getStolenCardList(){
+        return stolenCardService.getAllStolenCards();
+    }
+
 }
